1ca81ef69759bab67e497247f03dc609924ccba1
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-28 01:26:29 +0300

    Create PortScannerPage as StatefulWidge

---
+++ lib/PortScannerPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:network_tools/network_tools.dart';
+
+
+class PortScannerPage extends StatefulWidget {
+  const PortScannerPage({super.key});
+
+  @override
+  State<PortScannerPage> createState() => _PortScannerPageState();
+}
+
+class _PortScannerPageState extends State<PortScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
