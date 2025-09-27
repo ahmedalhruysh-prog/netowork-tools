8d6bd320ee211e6c582747fad2fbb7a2f6efa407
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-27 22:58:41 +0300

    add May class with materialApp and disable debug banner

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:networktool/PingPage.dart';
+import 'package:networktool/PortScannerPag.dart';
+import 'package:networktool/TraceroutePage.dart';
+import 'package:networktool/WhoisPage.dart';
+import 'package:whois/whois.dart';
+
+main(){
+  runApp(May());
+}
+class May extends StatelessWidget {
+  const May({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomePage() ,
+    );
+  }
+}
