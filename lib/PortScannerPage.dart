d43001ce847c2c0befe883f91bc541de01382a0a
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-28 01:29:59 +0300

    Add a map of well-known ports and their service names

--- lib/PortScannerPage.dart
+++ lib/PortScannerPage.dart
@@ -15,6 +15,23 @@ class _PortScannerPageState extends State<PortScannerPage> {
   String _result = "";
   bool _isScanning = false;
 
+  /// خريطة بأشهر المنافذ وأسمائها
+  final Map<int, String> _wellKnownPorts = {
+    20: 'FTP-Data',
+    21: 'FTP',
+    22: 'SSH',
+    23: 'Telnet',
+    25: 'SMTP',
+    53: 'DNS',
+    80: 'HTTP',
+    110: 'POP3',
+    143: 'IMAP',
+    443: 'HTTPS',
+    3306: 'MySQL',
+    3389: 'RDP',
+    5900: 'VNC',
+    8080: 'HTTP-Alt',
+  };
 
 
   @override
