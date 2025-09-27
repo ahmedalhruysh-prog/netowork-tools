8fbd609c176d2ac6c1275f7000d9ec86baeea354
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-28 01:58:09 +0300

    Create WhoisPage as StatefulWidget

---
+++ lib/WhoisPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:whois/whois.dart';
+
+class Whoispage extends StatefulWidget {
+  const Whoispage({super.key});
+
+  @override
+  State<Whoispage> createState() => _WhoispageState();
+}
+
+class _WhoispageState extends State<Whoispage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
