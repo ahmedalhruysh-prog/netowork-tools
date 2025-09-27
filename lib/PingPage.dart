1916f24c33ad8658afe5e73af2a33fdb600bfeab
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-28 00:10:23 +0300

    Add ElevatedButton to start ping

--- lib/PingPage.dart
+++ lib/PingPage.dart
@@ -6,8 +6,7 @@ class Pingpage extends StatefulWidget {
   @override
   State<Pingpage> createState() => _PingpageState();
 }
-
-class _PingpageState extends State<Pingpage> {
+ class _PingpageState extends State<Pingpage> {
   //متغير يخزن النتائج
   String _result = "";
 
@@ -18,33 +17,56 @@ class _PingpageState extends State<Pingpage> {
 
 
 
+// دالة بدء الـ Ping
+  void _startPing() {
+    final ip = _controller.text.trim(); // نجيب النص من خانة الإدخال
+    if (ip.isEmpty) {
+      setState(() {
+        _result = "⚠ الرجاء إدخال IP أو Domain أولاً\n";
+      });
+      return;
+    }
 
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+
+        ),
+
+
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              children: [
+                // مربع إدخال المستخدم
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: "أدخل IP أو Domain",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // زر بدء الـ Ping
+                ElevatedButton(
+                  onPressed: _startPing,
+                  child: Text("ابدأ Ping"),
+                ),
+                SizedBox(height: 20),
+
+              ]
+          ),
+        ),
+      );
+    }
+  }
 
   @override
-
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      title: Text("أداة Ping"),
-      backgroundColor: Colors.blue[900],
-
-    body: Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Column(
-    children: [
-    // مربع إدخال المستخدم
-    TextField(
-    controller: _controller,
-    decoration: InputDecoration(
-    labelText: "أدخل IP أو Domain",
-    border: OutlineInputBorder(),
-    ),
-    ),
-    SizedBox(height: 10),
-        ]
-    ),
-    ),
-    );
-
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
+
+
