d44d755d7121af1459275ec9ac6b7a0c1f361568
Ahmed ALhruysh <ahmedalhruysh@gmail.com>
2025-09-27 23:05:11 +0300

    Add Gridview layout to display tool options

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+
+        backgroundColor: Colors.indigoAccent,
+        title: Text("Network Tools",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
+      ),
+      body: Padding(padding: EdgeInsets.all(16),
+        child:GridView.count(crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 1,
+          children: [
+            ToolCard(title: "ping", color:Colors.blue, icon:Icons.wifi,page: PingPage(),),
+            ToolCard(title: "Traceroute", color:Colors.orange, icon: Icons.alt_route_rounded,page:TraceroutePage()),
+            ToolCard(title: "port", color:Colors.green, icon: Icons.shield_moon_rounded,page:PortScannerPage()),
+            ToolCard(title: "Whois", color:Colors.purple, icon:Icons.info_outline_rounded,page: WhoisPage(),),
+
+          ],
+        )
+        ,),
 
     );
   }
